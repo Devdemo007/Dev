c7944a3222b5ddfeef12d18130c869d055f26a52
Devdemo007 <hemansolution@gmail.com>
2016-07-15 14:24:17 +1000

    Add files via upload

---
+++ bar.js
@@ -0,0 +1,12 @@
+var foo = require('./foo');
+foo.f1(); // logs out : "a function in file foo"
+
+foo.f2(); // logs out : "a function in file foo"
+
+foo.f3(); // logs out : "a function in file foo"
+
+foo.f4(); // logs out : "a function in file foo"
+
+foo.f5(); // logs out : "a function in file foo"
+
+//1(); // logs out : "a function in file foo"
---
+++ foo.js
@@ -0,0 +1,19 @@
+exports.f1 = function () {
+    console.log('a function in file foo');
+};
+
+exports.f2 = function () {
+    console.log('a function in file foo');
+};
+
+exports.f3 = function () {
+    console.log('a function in file foo');
+};
+
+exports.f4 = function () {
+    console.log('a function in file foo');
+};
+
+exports.f5 = function () {
+    console.log('a function in file foo');
+};
